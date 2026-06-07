42f01ef2df325963863d56ecf28dcd511dc6ca9d
Lavigupta29 <lavigupta017@gmail.com>
2026-06-07 20:23:33 +0530

    Day-7 uploaded.

---
+++ Day-7/Question25.c
@@ -0,0 +1,30 @@
+// WAP to Recursive factorial
+
+#include<stdio.h>
+
+
+int factorial(int n){
+
+    if(n==0 || n==1){
+        return 1;
+    }
+    else{
+        return n * factorial(n-1);
+    }
+    
+}
+
+int main(){
+    int num, result;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    
+    result = factorial(num);
+
+    printf("Factorial of %d is %d\n", num, result);
+
+    return 0;
+
+}
+
---
+++ Day-7/Question27.c
@@ -0,0 +1,23 @@
+//WAP to Recursive sum of digits
+
+#include<stdio.h>
+
+int sumDigits(int n){
+    if(n==0){
+        return 0;
+    }
+    else{
+        return (n%10) + sumDigits(n/10);
+    }
+}
+
+int main(){
+    int n;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    printf("Sum of digits: %d\n", sumDigits(n));
+
+    return 0;
+}

2aa53f700dc1bb91110612063dab01b8ba46b126
Lavigupta29 <lavigupta017@gmail.com>
2026-06-08 15:48:03 +0530

    Day-8 uploaded.

---
+++ Day-8/Question31.c
@@ -0,0 +1,24 @@
+// WAP to Print character triangle
+
+#include<stdio.h>
+
+int main(){
+    
+    int i, j, rows;
+
+    printf("Enter no. of rows: ");
+    scanf("%d", &rows);
+
+    for(i=1; i<=rows; i++){
+
+        for(j=1; j<=i; j++){
+
+            printf("%c", 64 + j);
+            
+        }
+        printf("\n");
+    }
+
+    return 0;
+    
+}
